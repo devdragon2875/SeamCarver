b044afaa1d42bbc133157536d44b97dc7157c0d3
devdragon2875 <devanshmishra02@gmail.com>
2018-06-02 19:50:06 -0700

    Add files via upload

---
+++ SeamCarver/src/DisplayImage.java
@@ -0,0 +1,19 @@
+import java.awt.FlowLayout;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class DisplayImage {
+
+    
+	public DisplayImage() throws IOException
+    {
+        
+    }
+    
+}
